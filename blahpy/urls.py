4049295e880428abcbbc4c546f73eaaf4cd45916
Bill Koch <bkoch@mymainbox.local>
2012-05-21 17:18:29 -0400

    Modified archive view to show username for post and linked username to a profile page. created a profile view

--- blahpy/urls.py
+++ blahpy/urls.py
@@ -7,5 +7,6 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^blog/$', 'blog.views.index'),
 	url(r'^blog/archive/(?P<post_id>\d+)/$', 'blog.views.archive'),
+	url(r'^blog/profile/(?P<users_id>\d+)/$', 'blog.views.profile'),
     url(r'^admin/', include(admin.site.urls)),
 )
--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,25 @@
 from django.shortcuts import render_to_response
-from blog.models import Post
+from blog.models import Post, Users
 from django.http import Http404
 
 def index(request):
 	latest_post_list = Post.objects.all().order_by('-pub_date')[:5]
-	return render_to_response('blog/index.html', {'latest_post_list': latest_post_list})
+	u = Users.objects.all()
+	return render_to_response('blog/index.html', {'latest_post_list': latest_post_list,
+												   'user_info': u})
 
 def archive(request, post_id):
 	try:
 		p = Post.objects.get(pk=post_id)
-		u = Users.objects.get(pk=users_id)
+		u = Users.objects.get(pk=p.author_id)
 	except Post.DoesNotExist:
 		raise Http404
 	return render_to_response('blog/archive.html', {'post': p, 'author': u })
+
+def profile(request, users_id):
+	try:
+		user_info = Users.objects.get(pk=users_id)
+	except:
+		raise Http404
+	return render_to_response('blog/profile.html', {'user_info': user_info})
 
